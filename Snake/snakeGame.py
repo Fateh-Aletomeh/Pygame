9a3722a9ad0af0b633d164c125b20e292990f2dc
Fateh Aletomeh <86195398+Fateh-Aletomeh@users.noreply.github.com>
2024-10-20 21:58:18 +0100

    Update snakeGame.py
    Improved the code to use inheritance and split code into smaller functions to make it clearer and more efficient. Also added an option for the user to quit the game by pressing 'Q'.

--- Snake/snakeGame.py
+++ Snake/snakeGame.py
@@ -1,289 +1,208 @@
-from tkinter import messagebox
-from random import choice
 import pygame
 import random
-import tkinter
 
-root = tkinter.Tk()
-root.withdraw()
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+# Width and height must be multiples of 20
+WIDTH = 500
+HEIGHT = 500
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake")
 
-headImages = [pygame.image.load("snake_up.jpg"),
-              pygame.image.load("snake_down.jpg"),
-              pygame.image.load("snake_left.jpg"),
-              pygame.image.load("snake_right.jpg")]
+head_images = [pygame.image.load("snake_up.jpg"),
+               pygame.image.load("snake_right.jpg"),
+               pygame.image.load("snake_down.jpg"),
+               pygame.image.load("snake_left.jpg")]
 
 
-class head:
+class Point:
     def __init__(self, x, y):
-        self.height = 20
-        self.vel = 20
-        self.width = 20
         self.x = x
         self.y = y
 
-    def draw(self, win):
-        win.blit(headImages[direction], (self.x, self.y))
-
-
-class body:
-    def __init__(self, x, y):
-        self.height = 20
-        self.vel = 20
-        self.width = 20
-        self.x = x
-        self.y = y
-
-    def draw(self, win):
-        pygame.draw.rect(win, (199, 6, 7), (self.x, self.y, self.height, self.width))
-
-
-class snack:
-    def __init__(self, x, y):
-        self.height = 20
-        self.width = 20
-        self.x = x
-        self.y = y
-
-    def draw(self, win):
-        pygame.draw.rect(win, (51, 204, 51), (self.x, self.y, self.height, self.width))
-
-
-def checkHighScore():
-    highscore = open("highscore.txt")
-
-    if score > int(highscore.read()):
-        highscore = open("highscore.txt", "w")
-        highscore.write(str(score))
-
-
-def outputHighScore():
-    highscore = open("highscore.txt")
-    return highscore.read()
-
-
-def redrawGameWindow():
-    win.fill((22, 22, 24))
-    snakeHead.draw(win)
-    for snakeBody in bodies:
-        snakeBody.draw(win)
-    if len(foods) > 0:
-        food.draw(win)
-    pygame.display.update()
-
-
-# main loop
-up = down = left = False
-right = True
-run = True
-score = pos = oneLoop = 0
-foods = []
-bodies = []
-mem = []
-xy = []
-snakeHead = head(20, 20)
-while run:
-    pygame.time.delay(80)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    exception_x = []
-    exception_y = []
-
-    if len(foods) < 1:
-        food_x = 500 - random.randrange(0, 25) * 20 - 20
-        food_y = 500 - random.randrange(0, 25) * 20 - 20
-
-        for snakeBody in bodies:
-            xy[bodies.index(snakeBody)].append(snakeBody.x)
-            xy[bodies.index(snakeBody)].append(snakeBody.y)
-            if len(xy[bodies.index(snakeBody)]) == 4:
-                del xy[bodies.index(snakeBody)][:2]
-
-        for i in range(len(xy)):
-            if xy[i][0] == food_x and xy[i][1] == food_y:
-                ex_x = 24 - (xy[i][0] // 20)
-                ex_y = 24 - (xy[i][1] // 20)
-
-                exception_x.append(ex_x)
-                exception_y.append(ex_y)
-
-        if len(exception_x) > 0 and len(exception_y) > 0:
-            a = choice([i for i in range(0, 25) if i not in exception_x])
-            b = choice([i for i in range(0, 25) if i not in exception_y])
-
-            food_x = 500 - a * 20 - 20
-            food_y = 500 - b * 20 - 20
-
-        food = snack(food_x, food_y)
-        foods.append(food)
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_UP] and not down:
-        up = True
-        down = left = right = False
-    elif keys[pygame.K_DOWN] and not up:
-        down = True
-        up = left = right = False
-    elif keys[pygame.K_LEFT] and not right:
-        left = True
-        up = down = right = False
-    elif keys[pygame.K_RIGHT] and not left:
-        right = True
-        up = down = left = False
-
-    if up:
-        direction = 0
-        snakeHead.y -= snakeHead.vel
-        if snakeHead.y < 0:
-            snakeHead.y += 500
-    elif down:
-        direction = 1
-        snakeHead.y += snakeHead.vel
-        if snakeHead.y == 500:
-            snakeHead.y -= 500
-    elif left:
-        direction = 2
-        snakeHead.x -= snakeHead.vel
-        if snakeHead.x < 0:
-            snakeHead.x += 500
-    elif right:
-        direction = 3
-        snakeHead.x += snakeHead.vel
-        if snakeHead.x == 500:
-            snakeHead.x -= 500
-
-    if snakeHead.x == food.x and snakeHead.y == food.y and food in foods and len(bodies) == 0:
-        if len(bodies) == 0:
-            foods.remove(food)
-            score += 1
-            mem.append([])
-            xy.append([])
-            if up:
-                bodies.append(body(snakeHead.x, snakeHead.y + 20))
-            elif down:
-                bodies.append(body(snakeHead.x, snakeHead.y - 20))
-            elif left:
-                bodies.append(body(snakeHead.x + 20, snakeHead.y))
-            elif right:
-                bodies.append(body(snakeHead.x - 20, snakeHead.y))
-
-    for snakeBody in bodies:
-        if oneLoop == 1:
-            if up:
-                mem[0].append(0)
-            elif down:
-                mem[0].append(1)
-            elif left:
-                mem[0].append(2)
-            elif right:
-                mem[0].append(3)
-            oneLoop += 1
+    def move(self, direction):
+        if direction == 0:
+            self.y -= 20
+            if self.y < 0:
+                self.y += HEIGHT
+        elif direction == 1:
+            self.x += 20
+            if self.x == WIDTH:
+                self.x -= WIDTH
+        elif direction == 2:
+            self.y += 20
+            if self.y == HEIGHT:
+                self.y -= HEIGHT
         else:
-            if mem[bodies.index(snakeBody) - 1][0] == 0:
-                mem[bodies.index(snakeBody)].append(0)
-            elif mem[bodies.index(snakeBody) - 1][0] == 1:
-                mem[bodies.index(snakeBody)].append(1)
-            elif mem[bodies.index(snakeBody) - 1][0] == 2:
-                mem[bodies.index(snakeBody)].append(2)
-            elif mem[bodies.index(snakeBody) - 1][0] == 3:
-                mem[bodies.index(snakeBody)].append(3)
+            self.x -= 20
+            if self.x < 0:
+                self.x += WIDTH
 
-    oneLoop = 1
 
-    if len(mem) > 0:
-        if len(mem[0]) == 2:
-            for snakeBody in bodies:
-                if oneLoop == 1:
-                    if mem[0][0] == 0:
-                        snakeBody.y -= snakeBody.vel
-                        if snakeBody.y < 0:
-                            snakeBody.y += 500
-                    elif mem[0][0] == 1:
-                        snakeBody.y += snakeBody.vel
-                        if snakeBody.y == 500:
-                            snakeBody.y -= 500
-                    elif mem[0][0] == 2:
-                        snakeBody.x -= snakeBody.vel
-                        if snakeBody.x < 0:
-                            snakeBody.x += 500
-                    elif mem[0][0] == 3:
-                        snakeBody.x += snakeBody.vel
-                        if snakeBody.x == 500:
-                            snakeBody.x -= 500
+class Head(Point):
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
-                    oneLoop += 1
+    def draw(self, window, direction):
+        window.blit(head_images[direction], (self.x, self.y))
 
-                    if snakeHead.x == food.x and snakeHead.y == food.y and food in foods:
-                        if len(bodies) < 2:
-                            foods.remove(food)
-                            score += 1
-                            mem.append([])
-                            xy.append([])
-                            if mem[pos][0] == 0 or mem[pos][0] == 1 or mem[pos][0] == 2 or mem[pos][0] == 3:
-                                bodies.append(body(bodies[-1].x, bodies[-1].y))
-                                pos += 1
-                    mem[0].pop(0)
 
-        oneLoop = 1
+class Body(Point):
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
-        for snakeBody in bodies:
-            if len(mem[bodies.index(snakeBody)]) == 2:
-                if mem[bodies.index(snakeBody)][0] == 0:
-                    snakeBody.y -= snakeBody.vel
-                    if snakeBody.y < 0:
-                        snakeBody.y += 500
-                elif mem[bodies.index(snakeBody)][0] == 1:
-                    snakeBody.y += snakeBody.vel
-                    if snakeBody.y == 500:
-                        snakeBody.y -= 500
-                elif mem[bodies.index(snakeBody)][0] == 2:
-                    snakeBody.x -= snakeBody.vel
-                    if snakeBody.x < 0:
-                        snakeBody.x += 500
-                elif mem[bodies.index(snakeBody)][0] == 3:
-                    snakeBody.x += snakeBody.vel
-                    if snakeBody.x == 500:
-                        snakeBody.x -= 500
+    def draw(self, window):
+        pygame.draw.rect(window, (199, 6, 7), (self.x, self.y, 20, 20))
 
-                if snakeHead.x == food.x and snakeHead.y == food.y and food in foods and len(bodies) >= 3:
-                    foods.remove(food)
-                    score += 1
-                    mem.append([])
-                    xy.append([])
-                    if mem[pos][0] == 0:
-                        bodies.append(body(bodies[-1].x, bodies[-1].y - 20))
-                    if mem[pos][0] == 1:
-                        bodies.append(body(bodies[-1].x, bodies[-1].y + 20))
-                    if mem[pos][0] == 2:
-                        bodies.append(body(bodies[-1].x - 20, bodies[-1].y))
-                    if mem[pos][0] == 3:
-                        bodies.append(body(bodies[-1].x + 20, bodies[-1].y))
-                    pos += 1
 
-                if snakeHead.x == food.x and snakeHead.y == food.y and food in foods:
-                    foods.remove(food)
-                    score += 1
-                    mem.append([])
-                    xy.append([])
-                    if mem[pos][0] == 0 or mem[pos][0] == 1 or mem[pos][0] == 2 or mem[pos][0] == 3:
-                        bodies.append(body(bodies[-1].x, bodies[-1].y))
-                        pos += 1
+class Snack(Point):
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
-                mem[bodies.index(snakeBody)].pop(0)
+    def draw(self, window):
+        pygame.draw.rect(window, (51, 204, 51), (self.x, self.y, 20, 20))
 
-            if snakeBody.x == snakeHead.x and snakeBody.y == snakeHead.y:
-                run = False
 
-    redrawGameWindow()
+def readKeys(keys, direction):
+    if keys[pygame.K_UP] and direction != 2:
+        direction = 0
+    elif keys[pygame.K_RIGHT] and direction != 3:
+        direction = 1
+    elif keys[pygame.K_DOWN] and direction != 0:
+        direction = 2
+    elif keys[pygame.K_LEFT] and direction != 1:
+        direction = 3
 
-checkHighScore()
-pygame.quit()
-messagebox.showinfo("Game Over", f"Score: {score} \nHigh Score: {outputHighScore()}")
+    return direction
+
+
+def createNextBody(prev_x, prev_y, direction):
+    if direction == 0:
+        return Body(prev_x, prev_y + 20)
+    elif direction == 1:
+        return Body(prev_x - 20, prev_y)
+    elif direction == 2:
+        return Body(prev_x, prev_y - 20)
+    else:
+        return Body(prev_x + 20, prev_y)
+
+
+def checkDelay(length):
+    if length < 10:
+        return 100
+    elif length < 30:
+        return 80
+    elif length < 50:
+        return 70
+    elif length < 70:
+        return 60
+    elif length < 100:
+        return 55
+
+
+def checkHighScore(score):
+    file = open("highscore.txt")
+    if score > int(file.read()):
+        file = open("highscore.txt", "w")
+        file.write(str(score))
+    file.close()
+
+
+def outputHighScore(score):
+    from tkinter import messagebox
+    file = open("highscore.txt")
+    highscore = file.read()
+    file.close()
+    messagebox.showinfo("Game Over", f"Score: {score} \nHigh Score: {highscore}")
+
+
+def main():
+    head = Head(random.randint(1, WIDTH // 20 - 1) * 20, random.randint(1, HEIGHT // 20 - 1) * 20)
+    food = Snack(random.choice([i for i in range(0, WIDTH, 20) if i != head.x]),
+                 random.choice([i for i in range(0, HEIGHT, 20) if i != head.y]))
+    direction = random.randint(0, 3)
+    delay = 80
+    score = 0
+    bodies = []
+    memory = [direction]
+    exceptions_x = [head.x]
+    exceptions_y = [head.y]
+    ended_manually = False
+    game_running = True
+
+    while game_running:
+        pygame.time.delay(delay)
+
+        # Closes game if user clicks on close button (X) in top right corner of window or if user presses Q
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_q]:
+                game_running = False
+                ended_manually = True
+
+        # Check if food is eaten
+        if food.x == head.x and food.y == head.y:
+            score += 1
+            del food
+
+            # Update coordinate exceptions for food
+            exceptions_x[0] = head.x
+            exceptions_y[0] = head.y
+            for i in range(len(bodies)):
+                exceptions_x[i + 1] = bodies[i].x
+                exceptions_y[i + 1] = bodies[i].y
+
+            food = Snack(random.choice([i for i in range(0, WIDTH, 20) if i not in exceptions_x]),
+                         random.choice([i for i in range(0, HEIGHT, 20) if i not in exceptions_y]))
+
+            # Make snake longer
+            if len(bodies) > 0:
+                bodies.append(createNextBody(bodies[-1].x, bodies[-1].y, memory[-1]))
+            else:
+                bodies.append(createNextBody(head.x, head.y, memory[-1]))
+
+            exceptions_x.append(bodies[-1].x)
+            exceptions_y.append(bodies[-1].y)
+            memory.append(memory[-1])
+
+        # Set first index in memory of directions to direction of head
+        memory[0] = direction
+
+        # Read keys pressed by user and update direction
+        direction = readKeys(pygame.key.get_pressed(), direction)
+
+        # Move snake's head and body
+        head.move(direction)
+        for i in range(len(bodies)):
+            # Check if snake hits itself
+            if head.x == bodies[i].x and head.y == bodies[i].y:
+                game_running = False
+
+            # Move snake body
+            bodies[i].move(memory[i])
+
+        # Decrease delay of game (increase snake speed) as snake gets longer
+        delay = checkDelay(len(bodies))
+
+        # Update memory of directions
+        for i in range(1, len(memory)):
+            memory[-i] = memory[-i - 1]
+
+        # Redraw window
+        window.fill((22, 22, 24))
+        food.draw(window)
+        head.draw(window, direction)
+        for body in bodies:
+            body.draw(window)
+        pygame.display.update()
+
+    # Doesn't display score and highscore if user ends game manually
+    if not ended_manually:
+        checkHighScore(score)
+        outputHighScore(score)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
